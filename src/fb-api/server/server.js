54c1a917d87417aa40a308cf38ce9137aa03e6cc
Kydo <kydo@alliedworld.healthcare>
2019-09-11 05:33:41 +0000

    Merged in feature/fb_payment (pull request #28)
    Feature/fb payment
    
    * Initial commit for credit card support in FB notifications
    
    * Merge branch 'master' into feature/fb_payment
    
    * Fix package.json for FB API
    
    * Package.json reformat
    
    * Recommit fixes for FB API
    
    * Merge branch 'feature/fb_payment' of https://bitbucket.org/awhdatateam/notifications-pipeline into feature/fb_payment
    
    Approved-by: Philip Sales

--- src/fb-api/server/server.js
+++ src/fb-api/server/server.js
@@ -7,8 +7,15 @@ const swaggerDocument = require('../swagger.json');
 const fb = require('../fb/fb');
 const port = process.env.PORT;
 const app = express();
+//For testing only
+//const ngrok = require('ngrok');
+
+//fb.subRouter.restUrl = "http://172.104.54.251:3004";
+fb.subRouter.restUrl = "http://localhost:3004";
 
 app.use(cors())
+
+app.engine('html', require('ejs').renderFile); 
 
 app.use((req, res, next) => {
 	var now = new Date().toString();
@@ -21,10 +28,16 @@ app.use('/api-docs',
 	swaggerUi.serve, 
 	swaggerUi.setup(swaggerDocument));
 
-app.use('/fb', fb);
+app.use('/fb', fb.subRouter);
 
 app.listen(port, () => {
 	console.log(`Started on port ${process.env.HOST}:${port}`);
 });
 
+//For testing only
+/*(async function() {
+    const url = await ngrok.connect(3004);
+    console.log(url);
+    fb.subRouter.restUrl = url;
+})();*/
 module.exports = app;
